47e254e0b88c86c689b8c4d98a19031aa2b0fb44
Marc Bernardini <marcbernardini@gmail.com>
2014-05-31 14:03:08 -0600

    Merge branch 'master' of https://github.com/edrinkercope/Community-Garden-Spotter
    Conflicts:
    	public/stylesheets/style.css

--- public/javascripts/my.js
+++ public/javascripts/my.js
@@ -1,18 +1,51 @@
 $(function(){
+    var map =  $('#mapDiv');
+    var titleBar = $('#titlebar');
+    var filter = $('#filterNavigator');
+    var report = $('#report');
+    var helperWindow =  $('#helperWindow');
+    var filterNavigatorWindowSize = "200px";
+
+    var helperWindow = {
+        openFilter :{
+            headerText : "Let's start!",
+            bodyText : "Click on the shovel to begin your community gardening."
+        }
+    }
+
+    //Hide Windows
     $('#report').hide();
+    $('#helperWindow').hide();
+
+
+    //Hover
+    $('#titlebar span img').hover(function(evt){
+        var thisImg = $(this);
+        //alert("WORKS?");
+        thisImg.attr('src', '/assets/images/shovelGreenHover.png');
+
+    },
+                                  function(evt){
+                                      var thisImg = $(this);
+                                      thisImg.attr('src', '/assets/images/shovelGreen.png');
+                                  }
+                                 );
+
+    //Keypresses
     $('body').keypress(function(evt){
         console.log(evt.which);
+
         var showFilterNavigator = {
             left : "0"
 
         };
 
         var openFilterNavigator = {
-            left : "120px"
+            left : filterNavigatorWindowSize
         }
-        
+
         var hideFilterNavigator = {
-            left : "-120px"
+            left : "-"+filterNavigatorWindowSize
 
         };
 
@@ -20,22 +53,63 @@ $(function(){
             left : "0"
         }
 
+        var hideWindow = {
+            effect : "slide",
+            direction: "right"
+        }
+
+        var showWindow = {
+            effect : "slide",
+            direction: "left"
+        }
+
+        var mapVisible = $('#mapDiv').is(':visible');
+
         //By pressing 'o', this will open the filter navigator
         //window.
-        if(evt.which === 111 ){
-            $('#mapDiv').animate(openFilterNavigator);
-            $('#titleBar').animate(openFilterNavigator);
-            $('#filterNavigator').animate(showFilterNavigator);
+        if(evt.which === 111 && mapVisible){
+            map.animate(openFilterNavigator);
+            titleBar.animate(openFilterNavigator);
+            filter.animate(showFilterNavigator);
 
         }
-        
+
         //By pressing 'w', this will close the filter navigator
         //window.
-        if(evt.which === 119){
-            $('#mapDiv').animate(closeFilterNavigator);
-            $('#titleBar').animate(closeFilterNavigator);
-            $('#filterNavigator').animate(hideFilterNavigator);
+        if(evt.which === 119 && mapVisible){
+            map.animate(closeFilterNavigator);
+            titleBar.animate(closeFilterNavigator);
+            filter.animate(hideFilterNavigator);
+
+        }
+
+        //By pressing 'r', this will show the report.
+        if(evt.which === 114 && mapVisible){
+            map.hide(showWindow);
+            titleBar.hide(showWindow);
+            report.show(hideWindow);
+        }
+
+        //By pressing 'm', this will show the map
+        if(evt.which === 109){
+            map.show(showWindow);
+            titleBar.show(showWindow);
+            report.hide(hideWindow);
 
         }
     });
+
+
+    //Helper Functions
+    var buildHelpWindow = function(data){
+        helperWindow.empty();
+        var html = "";
+        html += "<h1 class='helperWindowHeader'>"+data.headerText+"</h1>";
+        html += "<p class='helperWindowText'>"+data.bodyText+"</p>";
+        helperWindow.html(html);
+
+    };
+
+
+
 });
