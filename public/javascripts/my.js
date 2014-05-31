d6e3b02d40073cae9f7ec299c20ca690b4c8af35
jovialgent <webauthor@MSAL7143s-MacBook-Pro.local>
2014-05-31 08:27:37 -0600

    added opening and closing the navigator
    This will allow the user to open and close the navigator window by
    pressing 'o' for open and 'w' for closing.

--- public/javascripts/my.js
+++ public/javascripts/my.js
@@ -1,4 +1,42 @@
 $(function(){
-    
-    
+    $('#report').hide();
+    $('body').keypress(function(evt){
+        console.log(evt.which);
+        var showFilterNavigator = {
+            left : "0"
+
+        };
+
+        var openFilterNavigator = {
+            left : "120px"
+        }
+        
+        var hideFilterNavigator = {
+            left : "-120px"
+
+        };
+
+        var closeFilterNavigator = {
+            left : "0"
+        }
+
+        //By pressing 'o', this will open the filter navigator
+        //window.
+        if(evt.which === 111 ){
+            $('#mapDiv').animate(openFilterNavigator);
+            $('#titleBar').animate(openFilterNavigator);
+            $('#filterNavigator').animate(showFilterNavigator);
+
+        }
+        
+        //By pressing 'w', this will close the filter navigator
+        //window.
+        if(evt.which === 119){
+            $('#mapDiv').animate(closeFilterNavigator);
+            $('#titleBar').animate(closeFilterNavigator);
+            $('#filterNavigator').animate(hideFilterNavigator);
+
+        }
+    });
+
 });
